dda8b5fbf79cd3b147fe8918fca92dedaecdd643
akash-kumar-dev <akashkumar.dev00@gmail.com>
2024-09-15 19:49:38 +0530

    Binary-Tree: height of a Tree, Node count, sum of Node, Diameter of a Tree

---
+++ ALPHA/14. Binary Tree/README.md
@@ -0,0 +1,7 @@
+## Binary Tree
+
+```
+- Binary Tree
+- Height of a Tree, Sum of Node, Diameter of Tree
+
+```
---
+++ ALPHA/14. Binary Tree/_2_height_sum_Dia_of_a_tree.java
@@ -0,0 +1,115 @@
+import java.util.*;
+public class _2_height_of_a_tree {
+
+    static class Node{
+        int data;
+        Node left, right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static int height(Node root) {
+        if(root == null) {
+            return 0;
+        }
+
+        int lh = height(root.left);
+        int rh = height(root.right);
+
+        return Math.max(lh, rh) + 1;
+    }
+
+    public static int count(Node root) {
+        if(root == null) {
+            return 0;
+        }
+
+        int leftCount = count(root.left);
+        int rightCount = count(root.right);
+
+        return leftCount + rightCount + 1;
+    }
+
+    // Sum of Nodes
+    public static int sum(Node root) {
+        if(root == null) {
+            return 0;
+        }
+
+        int leftSum = sum(root.left);
+        int rightSum = sum(root.right);
+        return leftSum + rightSum + root.data;
+    }
+
+    // Diameter of a Tree
+    public static int diameter(Node root) {     // O(n^2)
+        if(root == null) {
+            return 0;
+        }
+
+        int leftDiam = diameter(root.left);
+        int leftHt = height(root.left);
+        int rightDiam = diameter(root.right);
+        int rightHt = height(root.right);
+
+        int selfDiam = leftHt + rightHt + 1;
+
+        return Math.max(selfDiam, Math.max(leftDiam, rightDiam));
+    }
+
+    // Diameter of a tree (Approach 2)
+    static class Info {
+        int diam;
+        int ht;
+
+        public Info(int diam, int ht) {
+            this.diam = diam;
+            this.ht = ht;
+        }
+    }
+
+    public static Info diameter2(Node root) {   // O(n)
+        if(root == null) {
+            return new Info(0, 0);
+        }
+        Info leftInfo = diameter2(root.left);
+        Info rightInfo = diameter2(root.right);
+
+        int diam = Math.max(Math.max(leftInfo.diam, rightInfo.diam), leftInfo.ht + rightInfo.ht + 1);
+        int ht = Math.max(leftInfo.ht, rightInfo.ht) + 1;
+
+        return new Info(diam, ht);
+    }
+
+    public static void main(String[] args) {
+            /*
+             *              1
+             *             /  \
+             *            2    3
+             *           / \  / \
+             *          4  5  6  7
+             */
+
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.println(height(root));
+
+        System.out.println(count(root));
+
+        System.out.println(sum(root));
+
+        System.out.println(diameter(root));
+        System.out.println(diameter2(root).diam);
+    }
+
+}
