10b09ac95a780d3d1ae2d0012567c34275c37ac5
akash-kumar-dev <akashkumar.dev00@gmail.com>
2024-10-06 18:30:59 +0530

    Heaps: insert in Heap, peek, remove from Heap

---
+++ ALPHA/16. Heaps/_2_heaps.java
@@ -0,0 +1,94 @@
+import java.util.*;
+
+public class _2_heaps {
+    static class Heap {
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        // insert in Heap
+        public void add(int data) {
+            // add at Last idx
+            arr.add(data);
+
+            int x = arr.size() - 1;     // x is child index
+            int par = (x-1) / 2;        // parent idx
+
+            //  for maxHeap
+            // while(arr.get(x) > arr.get(par))
+            while(arr.get(x) < arr.get(par)) {      // O(logn)
+                // swap
+                int temp = arr.get(x);
+                arr.set(x, arr.get(par));
+                arr.set(par, temp);
+
+                x = par;
+                par = (x-1) / 2;
+            }
+        }
+
+        public int peek() {
+            return arr.get(0);
+        }
+
+
+        // Remove from Heap
+        private void heapify(int i) {
+            int left = 2*i + 1;
+            int right = 2*i + 2;
+            int minIdx = i;
+
+            // for maxHeap
+            // if(left < arr.size() && arr.get(minIdx) < arr.get(left)) {
+            if(left < arr.size() && arr.get(minIdx) > arr.get(left)) {
+                minIdx = left;
+            }
+
+            // for maxHeap
+            // if(right < arr.size() && arr.get(minIdx) < arr.get(right)) {
+            if(right < arr.size() && arr.get(minIdx) > arr.get(right)) {
+                minIdx = right;
+            }
+
+            if(minIdx != i) {
+                // swap
+                int temp = arr.get(i);
+                arr.set(i, arr.get(minIdx));
+                arr.set(minIdx, temp);
+
+                heapify(minIdx);
+            }
+        }
+        
+        public int remove() {
+            int data = arr.get(0);
+
+            // step1 - swap first & last
+            int temp = arr.get(0);
+            arr.set(0, arr.get(arr.size() - 1));
+            arr.set(arr.size() - 1, temp);
+
+            // step2 - delete last
+            arr.remove(arr.size() - 1);
+
+            // step3 - heapify
+            heapify(0);
+            return data;
+        }
+
+        public boolean isEmpty() {
+            return arr.size() == 0;
+        }
+        
+    }
+    public static void main(String[] args) {
+        Heap h = new Heap();
+        h.add(3);
+        h.add(4);
+        h.add(1);
+        h.add(5);
+        
+        while (!h.isEmpty()) {
+            System.out.println(h.peek());
+            h.remove();
+        }
+    }
+}
